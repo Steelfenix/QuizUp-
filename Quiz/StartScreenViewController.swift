f70d413589e9f1eee74cceb45659ca7d30c25013
Jose Adrian Castillo <32604482+Steelfenix@users.noreply.github.com>
2018-09-03 15:37:12 -0500

    Design Views

---
+++ Quiz/StartScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StartScreenViewController.swift
+//  Quiz
+//
+//  Created by Jose Adrian on 03/09/18.
+//  Copyright © 2018 Jose Adrian. All rights reserved.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
